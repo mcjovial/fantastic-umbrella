85f0993facc4314ac531295ca64203cbb6a982e4
mcjovial <emmanuelelias455@gmail.com>
2022-06-16 00:16:36 +0100

    pause & play fxn

--- script.js
+++ script.js
@@ -10,6 +10,59 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
+// Music
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Cover',
+  },
+  {
+    name: 'lazo-1',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Lazo Design',
+  },
+  {
+    name: 'song-1',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Song Artist',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
+
+// Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
